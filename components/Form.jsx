911aa892ca3f5fb0ec959f820722dfc0e774f669
Qudus Lawal <lawalqudus980@gmail.com>
2024-04-01 12:38:20 +0100

    updates

--- components/Form.jsx
+++ components/Form.jsx
@@ -8,13 +8,69 @@ const Form=()=>{
         email:"",
         password:""
     })
-    
-    
+
+    let firstname=document.getElementById('Firstname') 
+    let firstnamep=document.getElementById ('firstnamep')
+    let lastname=document.getElementById('Lastname') 
+    let lastnamep=document.getElementById ('lastnamep')
+    let gender=document.getElementById('Gender') 
+    let genderp=document.getElementById ('genderp')
+    let password=document.getElementById('Password') 
+    let passwordp=document.getElementById ('passwordp')
+    let email=document.getElementById('Email') 
+    let emailp=document.getElementById ('emailp')
+
+const info=[{
+    name: userInfo.firstName,
+    element: firstname,
+    paragraph: firstnamep
+},
+{
+    name: userInfo.lastName,
+    element: lastname,
+    paragraph: lastnamep
+},{
+    name: userInfo.gender,
+    element:gender,
+    paragraph: genderp
+}
+, {
+    name: userInfo.email,
+    element: email,
+    paragraph: emailp
+}, {
+    name: userInfo.password,
+    element:password,
+    paragraph: passwordp
+}]
+
 
     const handleSubmit=(e)=>{
         
-        e.preventDefault()
+                
+        
+        for(let i=0; i<info.length; i++){
+            if (info[i].name==="" || info[i].name===null ){
+            // info[i].element.style.borderStyle="2px red solid"
+            // firstname.style.borderColor="red"
+            info[i].paragraph.innerText="Field can't be empty"
+            info[i].element.style.borderColor="red"
+        } else{info[i].paragraph.innerText=""
+        info[i].element.style.borderColor="blue"}
 
+        if (info[i].name.length>0 &&  info[i].name.length<3){
+            // info[i].element.style.borderStyle="2px red solid"
+            // firstname.style.borderColor="red"
+            info[i].paragraph.innerText="Field can't be less than 3 characters"
+            info[i].element.style.borderColor="red"
+
+            if(info[i].name==="gender"){
+                continue
+            }
+        }  
+
+        }
+        e.preventDefault()
         console.log(userInfo);
 
         // console.log(userInfo.firstName, userInfo.lastName, userInfo.gender, userInfo.email, userInfo.password);
@@ -29,67 +85,92 @@ const Form=()=>{
                 [name]:value
             })
         )
+        // if ((firstname.value).length<3){
+        //     firstname.style.borderStyle="2px solid green"
+        //     // firstname.style.borderColor="red"
+        //     firstnamep.innerText="make valid input"
+        //     firstnamep.style.color="red"
+        // }
+
+        for(let i=0; i<info.length; i++){
+            if (info[i].name.length>0 && info[i].name.length<3 ){
+            // info[i].element.style.borderStyle="2px red solid"
+            // firstname.style.borderColor="red"
+            info[i].paragraph.innerText="input can't be less than 3 characters"
+            info[i].element.style.borderColor="red"
+        } else if(info[i].name.length<=0 || info[i].name.length>=3 ){
+            info[i].paragraph.innerText=""
+            info[i].element.style.borderColor="blue"}
+
+        // if (info[i].name.length>0 &&  info[i].name.length<3){
+        //     // info[i].element.style.borderStyle="2px red solid"
+        //     // firstname.style.borderColor="red"
+        //     info[i].paragraph.innerText="Field can't be less than 3 characters"
+        //     info[i].element.style.borderColor="red"
+
+        //     if(info[i].name==="gender"){
+        //         continue
+        //     }
+        // }  
+
+        }
         
     }
     return(
         <form onSubmit={handleSubmit}>
-            <h1>STUDENT REGISTRATION</h1>
+            <h2>STUDENT REGISTRATION</h2>
             <div className="form-control">
-                <label htmlFor="email">First Name</label>
-                <br />
-                <input id="firstname" name="firstName"
+                <label >First Name<input id="Firstname" name="firstName"
                 type="text"
                 placeholder="First Name"
                 value={userInfo.firstName}
                 onChange={handleInfo}
-                />
+                /></label>
+                <br />
+                
                 <p id="firstnamep"></p>
 
-                <br /><br />
-
-                <label htmlFor="lastName">Last Name</label>
-                <br />
-                <input id="lastname" name="lastName"
+                <label >Last Name<input id="Lastname" name="lastName"
                 type="text"
                 placeholder="Last Name"
                 value={userInfo.lastName}
                 onChange={handleInfo}
-                />
+                /></label>
+                <br />
+                
                 <p id="lastnamep"></p>
 
-                <br /><br />
 
-                <label htmlFor="gender">Gender</label>
-                <br />
-                <select name="gender" value={userInfo.gender} onChange={handleInfo}>
-                            <option selected disabled hidden id="gender">Gender</option>
+                <label >Gender<select id="Gender" name="gender" value={userInfo.gender} defaultValue={"Gender"} onChange={handleInfo}>
+                            <option value=" ">Gender</option>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
-                </select>
-                <br /><br />
-                <label htmlFor="email">Email</label>
+                </select></label>
                 <br />
-                <input id="email" name="email"
+                <p id="genderp"></p>
+                
+                <label>Email<input id="Email" name="email"
                 type="email"
                 placeholder="eg:name@gmail.com"
+                autoComplete="off"
                 value={userInfo.email}
                 onChange={handleInfo}
-                />
+                /></label>
+                <br />
+                
                 <p id="emailp"></p>
 
-
-                <br /><br />
-                <label htmlFor="password">Password</label>
-                <br />
-                <input id="password" name="password"
+                <label>Password<input id="Password" name="password"
                 type="password"
                 placeholder="*********"
                 value={userInfo.password}
                 onChange= {handleInfo}
-                />
+                /></label>
+                <br />
+                
                 <p id="passwordp"></p>
             </div>
-            <br /><br />
+            
             <button type="submit" >Submit</button>
 
         </form>
